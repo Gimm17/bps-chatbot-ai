3135e81d0798c1b1e0b42e1774d657b5966cbcf9
Gimm17 <agimpratamazte@gmail.com>
2026-08-18 15:55:35 +0800

    feat(bps): add bps webapi config + env scaffolding

---
+++ config/bps.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'key' => env('BPS_WEBAPI_KEY', ''),
+    'base_url' => env('BPS_WEBAPI_BASE_URL', 'https://webapi.bps.go.id'),
+    'enabled' => (bool) env('BPS_ENABLED', true),
+
+    'cache' => [
+        'enabled' => (bool) env('BPS_CACHE_ENABLED', true),
+        'ttl_hours' => (int) env('BPS_CACHE_TTL_HOURS', 24),
+        'prefix' => 'bps:',
+    ],
+
+    'agent' => [
+        'max_tool_calls' => (int) env('BPS_AGENT_MAX_TOOL_CALLS', 4),
+        'timeout_sec' => (int) env('BPS_AGENT_TIMEOUT_SEC', 60),
+    ],
+
+    'http' => [
+        'timeout_sec' => (int) env('BPS_HTTP_TIMEOUT_SEC', 15),
+    ],
+
+    'live_tests' => (bool) env('BPS_LIVE_TESTS', false),
+];
